df81f6d74fc94a9c6136b17b8d24e7ae5de7e07b
Bruno Tubino Franco <brunotubinofranco@gmail.com>
2026-03-09 20:31:15 -0300

    Create 01_join.sql

---
+++ exercicios/01_join.sql
@@ -0,0 +1,19 @@
+-- Quais clientes mais perderam pontos por Lover?
+
+SELECT t1.idCliente, 
+       t1.idTransacao,
+       t2.IdProduto,
+       t1.QtdePontos,
+       t3.DescNomeProduto,
+       t3.DescCategoriaProduto
+
+FROM transacoes AS t1
+
+LEFT JOIN transacao_produto AS t2
+ON t1.idTransacao = t2.IdTransacao
+
+LEFT JOIN produtos AS t3
+ON t2.IdProduto = t3.IdProduto
+
+WHERE t1.QtdePontos < 0 
+AND t3.DescCategoriaProduto LIKE 'lovers'
