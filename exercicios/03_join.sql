27d66cceb6d899725e939d92d85953b256488d48
Bruno Tubino Franco <brunotubinofranco@gmail.com>
2026-03-09 22:24:38 -0300

    Create 03_join.sql

---
+++ exercicios/03_join.sql
@@ -0,0 +1,18 @@
+-- Do início ao fim do nosso curso (2025/08/25 a 2025/08/29), quantos clientes assinaram 
+-- a lista de presença?
+
+SELECT COUNT(DISTINCT t1.IdCliente) AS qtdClientes, 
+       substr(t1.DtCriacao, 1, 10) AS data,
+       t3.DescNomeProduto
+
+FROM transacoes AS t1
+
+LEFT JOIN transacao_produto AS t2
+ON t1.IdTransacao = t2.IdTransacao
+
+LEFT JOIN produtos AS t3
+ON t2.IdProduto = t3.IdProduto
+
+WHERE substr(t1.DtCriacao, 1, 10) >= '2025-08-25'
+AND substr(t1.DtCriacao, 1, 10) < '2025-08-30'
+AND DescNomeProduto = 'Lista de presença'
