d76158baa4f67186855ec0d146df56a1f64b1642
Bruno Tubino Franco <brunotubinofranco@gmail.com>
2026-03-24 22:06:52 -0300

    Update 11_join_cte.sql

--- teoria/11_join_cte.sql
+++ teoria/11_join_cte.sql
@@ -38,8 +38,8 @@ ORDER BY qtdeDiasCurso DESC
 ) 
 
 SELECT CAST(AVG(qtdeDiasCurso) AS DOUBLE) AS mediaDiasCurso, 
-      MAX (qtdeDiasCurso),
-      MIN (qtdeDiasCurso)
+      MAX(qtdeDiasCurso),
+      MIN(qtdeDiasCurso)
 FROM tb_clientes_dias
 
 -- O resultado da consulta será a média de dias que os clientes que iniciaram o curso no primeiro dia estiveram presentes durante o curso.
